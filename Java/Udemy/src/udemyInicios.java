824045f161adaf5eb83d09fffb075dc970caf337
ejayaroa <ejayaroa@gmail.com>
2021-03-22 21:24:33 -0400

    Practicas basicas UDEMY

---
+++ Java/Udemy/src/udemyInicios.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jayaro
+ */
+public class udemyInicios {
+
+    public static void main(String[] args) {
+        var entera = 10;
+        var suma = 6;
+        var cadenas = "Saludos";
+        System.out.println(entera);
+        entera = 5;
+        System.out.println(entera);
+        System.out.println(cadenas + " \t" + (entera + suma));
+        System.out.println("cadenas = \"" + cadenas +"\"");
+
+    }
+}
