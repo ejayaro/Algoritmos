3cccc798b05e8d01dba6ad4d3d12bd4e145174cc
ejayaroa <ejayaroa@gmail.com>
2021-03-24 20:18:52 -0400

    Prosider 24032021

---
+++ Java/secuenciales/src/main/java/com/mycompany/secuenciales/secuenciales.java
@@ -0,0 +1,10 @@
+package com.mycompany.secuenciales;
+
+import java.util.Scanner;
+
+public class secuenciales {
+    public static void main(String[] args) {
+        float euros, dolares, libras, pesos, cambio;
+        euros = 1.1f; libras = 2.4f; 
+    }
+}
